e3173e7813ede7c441be12516c6ab1f88aff32be
JeongHoon Woo <frndhoon@gmail.com>
2024-10-01 22:48:44 +0900

    Title: 실수 입력받아 계산, Time: 153ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241001/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble(), b = input.nextDouble();
+        System.out.printf("%.2f", a + b);
+    }
+}
