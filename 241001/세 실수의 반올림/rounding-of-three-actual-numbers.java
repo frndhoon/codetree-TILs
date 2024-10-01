072394e6d0afb5b3452496e6a806127781b40176
JeongHoon Woo <frndhoon@gmail.com>
2024-10-01 22:50:18 +0900

    Title: 세 실수의 반올림, Time: 160ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241001/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble(), b = input.nextDouble(), c = input.nextDouble();
+        System.out.printf("%.3f\n", a);
+        System.out.printf("%.3f\n", b);
+        System.out.printf("%.3f\n", c);
+
+    }
+}
