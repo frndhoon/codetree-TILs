1d2f55dbe886188fbad4d9c6c4e2db6c29b4eeeb
JeongHoon Woo <frndhoon@gmail.com>
2024-10-01 22:56:32 +0900

    Title: 문자 받아 출력, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241001/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner input = new Scanner(System.in);
+        char message = input.next().charAt(0);
+        System.out.print(message);
+
+    }
+}
