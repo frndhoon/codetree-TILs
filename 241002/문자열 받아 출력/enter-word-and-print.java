f3d86b7227e5f5e3afbb8063a062d7cc340e1167
JeongHoon Woo <frndhoon@gmail.com>
2024-10-02 14:30:07 +0900

    Title: 문자열 받아 출력, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241002/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        System.out.print(s);
+
+    }
+}
