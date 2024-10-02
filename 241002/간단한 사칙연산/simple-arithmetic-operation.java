812defcccc49ebcbf9d864c93f6e0569079e5885
JeongHoon Woo <frndhoon@gmail.com>
2024-10-02 14:56:30 +0900

    Title: 간단한 사칙연산, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241002/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt(), b = input.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
