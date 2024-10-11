bc1319afadaa0e27249647f9b66938ba7a940d21
JeongHoon Woo <frndhoon@gmail.com>
2024-10-11 15:01:00 +0900

    Title: 2개의 정수를 서로 더하기, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241011/2개의 정수를 서로 더하기/add-two-integers-each-other.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]), b = Integer.parseInt(input[1]);
+        a += b;
+        b += a;
+        System.out.printf("%d %d", a, b);
+
+        
+    }
+}
