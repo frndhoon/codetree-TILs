ecfe6161d54a92a560858963b4c3201fd572971b
JeongHoon Woo <frndhoon@gmail.com>
2024-10-11 14:55:10 +0900

    Title: 몫과 나머지, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241011/몫과 나머지/quotient-remainder.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]), b = Integer.parseInt(input[1]);
+        System.out.printf("%d...%d", a/b, a%b);
+    }
+}
