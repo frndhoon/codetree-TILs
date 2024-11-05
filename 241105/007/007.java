b52a26603892a33db4bb16c73a72379f4daa2324
JeongHoon Woo <frndhoon@gmail.com>
2024-11-05 16:36:59 +0900

    Title: 007, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 241105/007/007.java
+++ 241105/007/007.java
@@ -1,18 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String code = st.nextToken();
+        String mettingPoint = st.nextToken();
+        int time = Integer.parseInt(st.nextToken());
 
         class CodeName {
             String code;
-            char meetingPoint;
+            String meetingPoint;
             int time;
 
-            public CodeName(String code, char meetingPoint, int time) {
+            public CodeName(String code, String meetingPoint, int time) {
                     this.code = code;
                     this.meetingPoint = meetingPoint;
                     this.time = time;
                 }
             };
-        CodeName code1 = new CodeName("codetree", 'L', 13);
+        CodeName code1 = new CodeName(code, mettingPoint, time);
         System.out.printf("secret code : %s\n", code1.code);
         System.out.printf("meeting point : %s\n", code1.meetingPoint);
         System.out.printf("time : %s", code1.time);
